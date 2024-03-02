0472749a398d0abf9d56e057493a11af94f17397
Aiko <59002254+aikobuny@users.noreply.github.com>
2024-03-03 06:17:25 +0800

    v1.0.5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 /*! Main Script */
 function loadFeature() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        let data = JSON.parse(xhttp.responseText);
+        console.log(data);
+    }
+    xhttp.open("GET", "api/featured.json");
+    xhttp.send();
+}
+
+function loadNews() {
     ;
 }
