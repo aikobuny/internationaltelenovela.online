6b5c3fcfb952c11cd38b02a249446d3e952a01b8
Aiko <59002254+aikobuny@users.noreply.github.com>
2024-03-03 23:29:57 +0800

    v1.0.82

--- js/main.js
+++ js/main.js
@@ -3,18 +3,21 @@ function loadPrograms(type) {
     let xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
         let data = JSON.parse(xhttp.responseText);
+        let programs_count = data.length;
         console.log(data);
         if (type == "featured") {
-            let a = document.getElementsByClassName("programs_featured")[0];
-            let box = document.createElement('div');
-            box.classList.add('box');
-            a.appendChild(box);
-            let thumbnail_box = document.createElement('div');
-            thumbnail_box.classList.add('thumbnail-box')
-            box.appendChild(thumbnail_box);
-            let img = document.createElement('img');
-            img.setAttribute("src", data["thumbnail"])
-            thumbnail_box.appendChild(img);
+            for (let i = 0; i < programs_count; i++) {
+                let a = document.getElementsByClassName("programs_featured")[0];
+                let box = document.createElement('div');
+                box.classList.add('box');
+                a.appendChild(box);
+                let thumbnail_box = document.createElement('div');
+                thumbnail_box.classList.add('thumbnail-box')
+                box.appendChild(thumbnail_box);
+                let img = document.createElement('img');
+                img.setAttribute("src", data[i]["thumbnail"])
+                thumbnail_box.appendChild(img);
+            }
         }
     }
     if (type == "featured") {
