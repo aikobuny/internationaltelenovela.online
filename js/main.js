d98a7968dc18ed926cb8252916359228f1d6d05f
Aiko <59002254+aikobuny@users.noreply.github.com>
2024-03-04 00:05:01 +0800

    v1.0.85

--- js/main.js
+++ js/main.js
@@ -2,22 +2,34 @@
 function loadPrograms(type) {
     let xhttp = new XMLHttpRequest();
     let data;
+    let selection;
     xhttp.onload = function() {
         data = JSON.parse(xhttp.responseText);
         let programs_count = data.length;
         if (type == "featured") {
-            for (let i = 0; i < programs_count; i++) {
-                let a = document.getElementsByClassName("programs_featured")[0];
-                let box = document.createElement('div');
-                box.classList.add('box');
-                a.appendChild(box);
-                let thumbnail_box = document.createElement('div');
-                thumbnail_box.classList.add('thumbnail-box')
-                box.appendChild(thumbnail_box);
-                let img = document.createElement('img');
-                img.setAttribute("src", data[i]["thumbnail"])
-                thumbnail_box.appendChild(img);
-            }
+            selection = "programs_featured";
+        }
+        for (let i = 0; i < programs_count; i++) {
+            let a = document.getElementsByClassName(selection)[0];
+            let box = document.createElement("div");
+            box.classList.add("box");
+            a.appendChild(box);
+            let thumbnail_box = document.createElement("div");
+            thumbnail_box.classList.add("thumbnail-box")
+            box.appendChild(thumbnail_box);
+            let img = document.createElement("img");
+            img.setAttribute("src", data[i]["thumbnail"])
+            thumbnail_box.appendChild(img);
+
+            let detail_box = document.createElement("div");
+            detail_box.classList.add("detail-box");
+            box.appendChild(detail_box);
+            let h5 = document.createElement("h5");
+            h5.innerHTML = data[i]["name"];
+            detail_box.appendChild(h5);
+            let p = document.createElement("p");
+            p.innerHTML = data[i]["desc"]
+            detail_box.appendChild(p)
         }
     }
     if (type == "featured") {
