5724158b3c6f9131daadac6261e6b899635caf97
Aiko <59002254+aikobuny@users.noreply.github.com>
2024-03-03 23:08:40 +0800

    v1.0.81

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 /*! Main Script */
-function loadFeature() {
+function loadPrograms(type) {
     let xhttp = new XMLHttpRequest();
     xhttp.onload = function() {
         let data = JSON.parse(xhttp.responseText);
         console.log(data);
+        if (type == "featured") {
+            let a = document.getElementsByClassName("programs_featured")[0];
+            let box = document.createElement('div');
+            box.classList.add('box');
+            a.appendChild(box);
+            let thumbnail_box = document.createElement('div');
+            thumbnail_box.classList.add('thumbnail-box')
+            box.appendChild(thumbnail_box);
+            let img = document.createElement('img');
+            img.setAttribute("src", data["thumbnail"])
+            thumbnail_box.appendChild(img);
+        }
     }
-    xhttp.open("GET", "api/featured.json");
+    if (type == "featured") {
+        xhttp.open("GET", "api/featured.json");
+    }
     xhttp.send();
 }
 
