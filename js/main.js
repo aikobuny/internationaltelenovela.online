a2ab0f850f1104c17f5d05a7105346d2d7b03618
Aiko <59002254+aikobuny@users.noreply.github.com>
2024-03-04 01:26:30 +0800

    1.0.87

--- js/main.js
+++ js/main.js
@@ -2,39 +2,45 @@
 function loadPrograms(type) {
     let xhttp = new XMLHttpRequest();
     let data;
-    let selection;
+    let selection = [];
     xhttp.onload = function() {
         data = JSON.parse(xhttp.responseText);
         let programs_count = data.length;
-        for (let i = 0; i < programs_count; i++) {
-            let a = document.getElementsByClassName(selection)[0];
-            let box = document.createElement("div");
-            box.classList.add("box");
-            a.appendChild(box);
-            let thumbnail_box = document.createElement("div");
-            thumbnail_box.classList.add("thumbnail-box")
-            box.appendChild(thumbnail_box);
-            let img = document.createElement("img");
-            img.setAttribute("src", data[i]["thumbnail"])
-            thumbnail_box.appendChild(img);
-
-            let detail_box = document.createElement("div");
-            detail_box.classList.add("detail-box");
-            box.appendChild(detail_box);
-            let h5 = document.createElement("h5");
-            h5.innerHTML = data[i]["name"];
-            detail_box.appendChild(h5);
-            let p = document.createElement("p");
-            p.innerHTML = data[i]["desc"]
-            detail_box.appendChild(p)
+        for (let _i = 0; i < selection.length; i++) {
+            for (let i = 0; i < programs_count; i++) {
+                let a = document.getElementsByClassName(selection[_i])[0];
+                let box = document.createElement("div");
+                box.classList.add("box");
+                a.appendChild(box);
+                let thumbnail_box = document.createElement("div");
+                thumbnail_box.classList.add("thumbnail-box")
+                box.appendChild(thumbnail_box);
+                let img = document.createElement("img");
+                img.setAttribute("src", data[i]["thumbnail"])
+                thumbnail_box.appendChild(img);
+    
+                let detail_box = document.createElement("div");
+                detail_box.classList.add("detail-box");
+                box.appendChild(detail_box);
+                let h5 = document.createElement("h5");
+                h5.innerHTML = data[i]["name"];
+                detail_box.appendChild(h5);
+                let p = document.createElement("p");
+                p.innerHTML = data[i]["desc"]
+                detail_box.appendChild(p)
+            }
         }
     }
     if (type == "featured") {
-        selection = "programs_featured";
+        selection = [
+            "programs_featured"
+        ];
         xhttp.open("GET", "api/featured.json");
     }
     if (type == "all") {
-        selection = "programs_all";
+        selection = [
+            "programs_my"
+        ];
         xhttp.open("GET", "api/programs.json")
     }
     xhttp.send();
