5eeaa3c10ce92449d26639320fad8fdf536e0ddf
Aiko <59002254+aikobuny@users.noreply.github.com>
2024-03-04 01:35:56 +0800

    v1.0.87

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@ function loadPrograms(type) {
     let selection = [];
     xhttp.onload = function() {
         data = JSON.parse(xhttp.responseText);
-        let programs_count = data.length;
-        for (let _i = 0; i < selection.length; i++) {
+        if (type == "featured") {
+            let programs_count = data.length;
             for (let i = 0; i < programs_count; i++) {
-                let a = document.getElementsByClassName(selection[_i])[0];
+                let a = document.getElementsByClassName(selection[0])[0];
                 let box = document.createElement("div");
                 box.classList.add("box");
                 a.appendChild(box);
@@ -29,7 +29,35 @@ function loadPrograms(type) {
                 p.innerHTML = data[i]["desc"]
                 detail_box.appendChild(p)
             }
+        } else {
+            for (let _i = 0; i < selection.length; i++) {
+                let country = selection[_i].slice(-2)
+                let programs_count = data[country].length;
+                for (let i = 0; i < programs_count; i++) {
+                    let a = document.getElementsByClassName(selection[_i])[0];
+                    let box = document.createElement("div");
+                    box.classList.add("box");
+                    a.appendChild(box);
+                    let thumbnail_box = document.createElement("div");
+                    thumbnail_box.classList.add("thumbnail-box")
+                    box.appendChild(thumbnail_box);
+                    let img = document.createElement("img");
+                    img.setAttribute("src", data[country][i]["thumbnail"])
+                    thumbnail_box.appendChild(img);
+        
+                    let detail_box = document.createElement("div");
+                    detail_box.classList.add("detail-box");
+                    box.appendChild(detail_box);
+                    let h5 = document.createElement("h5");
+                    h5.innerHTML = data[country][i]["name"];
+                    detail_box.appendChild(h5);
+                    let p = document.createElement("p");
+                    p.innerHTML = data[country][i]["desc"]
+                    detail_box.appendChild(p)
+                }
+            }
         }
+        
     }
     if (type == "featured") {
         selection = [
@@ -39,7 +67,7 @@ function loadPrograms(type) {
     }
     if (type == "all") {
         selection = [
-            "programs_my"
+            "programs_MY"
         ];
         xhttp.open("GET", "api/programs.json")
     }
